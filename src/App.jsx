0f1a63d8e333f521889e4fadd5eae77f6918386a
Ayush Rai <202438056+ayushraistudio@users.noreply.github.com>
2026-01-18 13:04:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Card from "./Card.jsx";
+import MoreAboutMe from "./MoreAboutMe.jsx";
 
 function App() {
-  return <Card />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Card />} />
+        <Route path="/about" element={<MoreAboutMe />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
